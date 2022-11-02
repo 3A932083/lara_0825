314288275e6ed7c0f112f94119d4b5b66410f64b
jiang shu yi <3a932083@gm.student.ncut.edu.tw>
2022-11-02 16:00:10 +0800

    建立主樣板

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('title')</title><!--將來個別網頁(也稱子樣板或 view)的 'title' 可以置入此位置-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        @include('layouts.partials.navigation')<!--使得navigation.blade.php當中Navigation 區塊可以引入-->
+
+        <!-- Page Header-->
+        <!-- Main Content-->
+        @yield('content')<!-- 將來個別網頁(也稱子樣板或 view)當中的 Header 區塊與 Content 區塊可以置入此位置-->
+
+        <!-- Footer-->
+        @include('layouts.partials.footer')<!--使得 footer.blade.php 當中 Footer 區塊可以引入-->
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
