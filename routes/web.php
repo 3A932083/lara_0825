c972276d5aa0ee937ce89078ff5970a8dfa5d279
jiang shu yi <3a932083@gm.student.ncut.edu.tw>
2022-11-16 23:09:13 +0800

    用post()擷取所屬的貼文

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,29 @@ use App\Models\Comment;
 
 Route::get('/', function () {
 
+    /*
+    echo'用comments()擷取該post所有評論'.'<br>';
     $post = Post::find(8);
-    echo'標題'.$post->title.'<br>';
-    echo'內容'.$post->content.'<br>';
-    echo'-----------------'.'<br>';
+    echo'標題：'.$post->title.'<br>';
+    echo'內容：'.$post->content.'<br>';
+    echo'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.'<br>';
     $comments = $post->comments;
     foreach ($comments as $comment){
-        echo'留言'.$comment->content.'<br>';
-        echo'-----------------'.'<br>';
-    }
+        echo'留言：'.$comment->content.'<br>';
+        echo'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.'<br>';
+    }*/
+
+
+    //echo'<br>';
+    //echo'用post()擷取所屬的貼文'.'<br>';
+    $comment = Comment::find(6);
+    echo '內容：'.$comment->content.'<br>';
+    echo'---------------------------------------------------------'.'<br>';
+    $post = $comment->post;
+    echo '編號：'.$post->id.'<br>';
+    echo '標題：'.$post->title.'<br>';
+    echo '內容：'.$post->content.'<br>';
+
 });
 
 Route::get('posts',[PostController::class,'index'])->name('posts.index');
